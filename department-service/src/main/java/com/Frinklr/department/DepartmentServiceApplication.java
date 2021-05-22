3361169d2c82117b3f1dfa61d4d00d49ca40bae2
Vaibhav <vaibhavchhabra800@gmail.com>
2021-05-22 23:50:20 +0530

    Eureka Service stared working

--- department-service/src/main/java/com/Frinklr/department/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/Frinklr/department/DepartmentServiceApplication.java
@@ -1,10 +1,10 @@
 package com.Frinklr.department;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
